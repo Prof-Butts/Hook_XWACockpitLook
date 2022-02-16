3a7534f076a5ed3696663168ccb9194df4e94382
morallo <m_orallo@yahoo.es>
2022-02-17 00:58:47 +0100

    Fix external camera in hangar

--- hooks.h
+++ hooks.h
@@ -9,6 +9,8 @@ static const HookFunction g_hookFunctions[] =
 	{ 0x4EE9DA, UpdateCameraTransformHook }, //UpdateCameraTransform for cockpit view
 	{ 0x4EEB56, UpdateCameraTransformHook }, //UpdateCameraTransform for external camera
 	{ 0x4EF044, UpdateCameraTransformHook }, //UpdateCameraTransform for no cockpit camera
+	{ 0x459AC7, UpdateCameraTransformHook }, //UpdateCameraTransform for hangar external camera
+	{ 0x4A131C, UpdateCameraTransformHook }, //UpdateCameraTransform for map
 	{ 0x497ABB, CockpitPositionTransformHook },
 	{ 0x43FB57, DoRotationPitchHook}, //DoRotation(Pitch)
 	{ 0x43FB67, DoRotationYawHook }, //DoRotation(Yaw)
@@ -25,10 +27,18 @@ static const HookPatchItem g_patch[] =
 	// Hook calls to UpdateCameraTransform inside PlayerCameraUpdate()
 	// call (0x5A8B20 - 0x4EE9DA) = 0x0BA146
 	{ 0x4EE9D5 - 0x400C00, "E8060FF5FF", "E846A10B00"},
-	// call (0x5A8B20 - 0x4EEB56) = 0x0B9FCF
+	// call (0x5A8B20 - 0x4EEB56) = 0x0B9FCA
 	{ 0x4EEB51 - 0x400C00, "E88A0DF5FF", "E8CA9F0B00"},
-	// call (0x5A8B20 - 0x4EF044) = 0x0B9FCF
+	// call (0x5A8B20 - 0x4EF044) = 0x0B9ADC
 	{ 0x4EF03F - 0x400C00, "E89C08F5FF", "E8DC9A0B00"},
+	
+	// Hook calls to UpdateCameraTransform inside DrawHangarView()
+	// call (0x5A8B20 - 0x459AC7) = 0x14F059
+	{ 0x459AC2 - 0x400C00, "E8195EFEFF", "E859F01400"},
+
+	// Hook calls to UpdateCameraTransform inside RotateCameraInMap()
+	// call (0x5A8B20 - 0x4A131C) = 0x107804
+	{ 0x4A1317 - 0x400C00, "E8C4E5F9FF", "E804781000"},
 
 	// Hook last call to Vector3Transform() inside UpdatePlayerMovement() that provides the values for CockpitPositionTransformed.
 	// To add positional tracking. This only works in cockpit view mode!
